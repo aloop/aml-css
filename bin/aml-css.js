d45c949f504997e3c3de75af4f4d369b56c3c47a
Anthony Loop <anthony@anthonyloop.com>
2023-10-14 21:17:24 -0700

    alter indent size to 2 from 4

--- bin/aml-css.js
+++ bin/aml-css.js
@@ -22,12 +22,12 @@ const createOutputFlagRegex = /^-o$|^--output$|^--output=(?<output>.*)$/u;
 // Functions
 
 function error(...messages) {
-    console.error(...messages);
-    process.exit();
+  console.error(...messages);
+  process.exit();
 }
 
 function help(err = false) {
-    const text = `Usage:
+  const text = `Usage:
 
     ${path.basename(_filename)} <command> [OPTION...]
 
@@ -43,56 +43,56 @@ function help(err = false) {
     -h, --help         Print this help text
   `;
 
-    if (err) {
-        console.error(text);
-    } else {
-        console.log(text);
-    }
+  if (err) {
+    console.error(text);
+  } else {
+    console.log(text);
+  }
 }
 
 function copyDir(src, dest) {
-    if (!fs.existsSync(src)) {
-        error(`Src dir "${src}" doesn't exist`);
-    }
+  if (!fs.existsSync(src)) {
+    error(`Src dir "${src}" doesn't exist`);
+  }
 
-    if (fs.lstatSync(src).isDirectory()) {
-        fs.mkdirSync(dest, { recursive: true });
-        for (const child of fs.readdirSync(src)) {
-            copyDir(path.resolve(src, child), path.resolve(dest, child));
-        }
-    } else {
-        fs.copyFileSync(src, dest);
+  if (fs.lstatSync(src).isDirectory()) {
+    fs.mkdirSync(dest, { recursive: true });
+    for (const child of fs.readdirSync(src)) {
+      copyDir(path.resolve(src, child), path.resolve(dest, child));
     }
+  } else {
+    fs.copyFileSync(src, dest);
+  }
 }
 
 // Commands and flags
 
 const commands = {
-    create(args) {
-        for (let i = 0; i < args.length; i++) {
-            const arg = args[i];
+  create(args) {
+    for (let i = 0; i < args.length; i++) {
+      const arg = args[i];
 
-            const match = arg.match(createOutputFlagRegex);
-            if (match !== null) {
-                if (match.groups.output?.length < 1) {
-                    error("No value given to --output flag. Exiting...");
-                } else if (match.groups.output?.length > 0) {
-                    outputPath = path.resolve(workingPath, match.groups.output);
-                } else {
-                    // If we've hit this branch, we need to check the next value.
-                    i++;
+      const match = arg.match(createOutputFlagRegex);
+      if (match !== null) {
+        if (match.groups.output?.length < 1) {
+          error("No value given to --output flag. Exiting...");
+        } else if (match.groups.output?.length > 0) {
+          outputPath = path.resolve(workingPath, match.groups.output);
+        } else {
+          // If we've hit this branch, we need to check the next value.
+          i++;
 
-                    if (i + 1 > args.length || args[i].startsWith("-")) {
-                        error("No value given to --output flag. Exiting...");
-                    }
+          if (i + 1 > args.length || args[i].startsWith("-")) {
+            error("No value given to --output flag. Exiting...");
+          }
 
-                    outputPath = path.resolve(workingPath, args[i]);
-                }
-            }
+          outputPath = path.resolve(workingPath, args[i]);
         }
+      }
+    }
 
-        copyDir(scaffoldPath, outputPath);
-    },
+    copyDir(scaffoldPath, outputPath);
+  },
 };
 
 const flags = {};
@@ -102,19 +102,19 @@ const flags = {};
 const args = process.argv.slice(2);
 
 if (args.includes("-h") || args.includes("--help")) {
-    help();
-    process.exit();
+  help();
+  process.exit();
 }
 
 for (let i = 0; i < args.length; i++) {
-    const arg = args[i];
+  const arg = args[i];
 
-    if (arg.startsWith("-") && flags.hasOwnProperty(arg)) {
-        flags[arg]();
-    } else if (commands.hasOwnProperty(arg)) {
-        commands[arg](args.slice(i + 1));
-        process.exit();
-    }
+  if (arg.startsWith("-") && flags.hasOwnProperty(arg)) {
+    flags[arg]();
+  } else if (commands.hasOwnProperty(arg)) {
+    commands[arg](args.slice(i + 1));
+    process.exit();
+  }
 }
 
 // If we haven't executed any commands print the help message to STDERR and exit
